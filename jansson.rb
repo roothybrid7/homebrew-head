ed089e825813b7aaa5904000aaf6bcdbe7f84942
Satoshi Ohki <roothybrid7@gmail.com>
2013-07-15 15:49:55 +0900

    Remove stable version

--- jansson.rb
+++ jansson.rb
@@ -2,9 +2,6 @@ require 'formula'
 
 class Jansson < Formula
   homepage 'http://www.digip.org/jansson/'
-  url 'http://www.digip.org/jansson/releases/jansson-2.4.tar.bz2'
-  sha1 'f11ff89dd484d89210d9f52a9ccc2ce2b9d2c1ae'
-
   head 'https://github.com/akheron/jansson.git', :using => :git
 
   option :universal
